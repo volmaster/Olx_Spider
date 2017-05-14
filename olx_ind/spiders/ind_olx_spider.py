4088004b8642d06fa1a8aecebd58890ab70418a7
volmaster <volmasters@gmail.com>
2017-05-14 13:57:57 +0300

    overwritten start_requests method

--- olx_ind/spiders/ind_olx_spider.py
+++ olx_ind/spiders/ind_olx_spider.py
@@ -1,15 +1,21 @@
 # -*- coding: utf-8 -*-
 import scrapy
 from scrapy_splash import SplashRequest
+from olx_ind.items import OlxIndItem
 from PIL import Image
 from io import BytesIO
 import base64
 from pytesseract import image_to_string
 
 
-class IndOlxSpider(scrapy.Spider):
-    name = "indspider"
-    start_urls = ['http://olx.co.id/perlengkapan-bayi-anak/jakarta-dki/?view=list']
+class OlxIndSpider(scrapy.Spider):
+    name = "olxindspider"
+
+    def start_requests(self):
+        url = 'http://olx.co.id/perlengkapan-bayi-anak/jakarta-dki/?view=list&page={}'
+        # Make request to a first 10 pages
+        for i in range(1, 11):
+            yield self.make_requests_from_url(url.format(i))
 
     def parse(self, response):
         script = """
@@ -18,7 +24,6 @@ class IndOlxSpider(scrapy.Spider):
                           assert(splash:go(url))
                           assert(splash:wait(1))
                           assert(splash:runjs("$('.contactitem').click()"))
-                          --assert(splash:runjs("$('.fnormal').click()"))
                           assert(splash:wait(1))
                           return {
                             html = splash:html()
@@ -30,16 +35,15 @@ class IndOlxSpider(scrapy.Spider):
                                 args={'lua_source': script})
 
     def parse_phone(self, response):
-        item = dict()
+        item = OlxIndItem()
         item['url'] = response.url
         item['item_name'] = response.xpath('//h1/text()').extract_first().strip()
         item['seller_name'] = response.xpath('(//span[contains(@class, "block color")])[1]/text()').extract_first()
         raw_image = response.xpath('//img[@class="contactimg"]/@src').extract_first()
         try:
             image = Image.open(BytesIO(base64.b64decode(raw_image.split(',')[1])))
-            item['phone_number'] = image_to_string(image)
+            item['phone_number'] = ''.join(image_to_string(image).split())
             yield item
         except Exception as e:
-            self.logger.info('Got {} exeption on {} page'.format(e, response.url))
+            self.logger.info('Exeption {} on {} page'.format(e, response.url))
             pass
-
